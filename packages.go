60dbd285ef4a721c82a7f223316e752f8d6f8c07
Colin <366760348@qq.com>
2015-09-07 09:29:09 +0800

    test

---
+++ packages.go
@@ -0,0 +1,23 @@
+package main
+2
+ 
+ 3
+ import (
+ 4
+     "fmt"
+     5
+         "math/rand"
+         6
+
+ )
+ 7
+  
+  8
+  func main() {
+      9
+          fmt.Println("My favorite number is", rand.Intn(10))
+         10
+
+  }
+  11
+   
